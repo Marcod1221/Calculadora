dd7c55d8d87c84e822238abc7c0a74a1e2195920
MarcodPG98 <mg32834@gmail.com>
2022-11-12 08:37:09 -0600

    validar datos

--- marcaje_frontend/src/app/app.module.ts
+++ marcaje_frontend/src/app/app.module.ts
@@ -4,13 +4,8 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
-import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { MarcajeComponent } from './components/marcaje/marcaje.component';
 import { CaculadoraComponent } from './datos/caculadora/caculadora.component';
 
 @NgModule({
@@ -18,11 +13,6 @@ import { CaculadoraComponent } from './datos/caculadora/caculadora.component';
     AppComponent,
     NavbarComponent,
     LoginComponent,
-    RegisterComponent,
-    ProfileComponent,
-    RequestResetComponent,
-    ResponseResetComponent,
-    MarcajeComponent,
     CaculadoraComponent
   ],
   imports: [
